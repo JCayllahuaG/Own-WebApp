81f402ab921acb36fe2819b5f5187682d272fa6c
JC <u202019558@upc.edu.pe>
2023-10-24 01:10:10 -0500

    feat(component): dialog to add new activities

---
+++ src/app/experience_design/components/add-activity-dialog/add-activity-dialog.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-add-activity-dialog',
+  templateUrl: './add-activity-dialog.component.html',
+  styleUrls: ['./add-activity-dialog.component.scss']
+})
+export class AddActivityDialogComponent {
+}
